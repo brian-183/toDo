16de450f46b8fb7d567dee204c3b74dd1e6cadd2
EC2 Default User <ec2-user@ip-172-31-86-46.ec2.internal>
2019-10-23 20:56:48 +0000

    my first repo

---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+Task.create([
+    {title: "Drop it", details: "whatever"},
+    {title: "Go to store", details: "Get some cheesus from yeezus"},
+    {title: "Yandhi", details: "Trying to test my ..."},
+    {title: "holidays", details: "go to spain"}
+    ])
